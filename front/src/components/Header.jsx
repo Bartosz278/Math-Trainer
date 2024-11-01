f4aaa59443213ebdacc8eff08f4da679a59baa2f
Bartosz <80819432+Bartosz278@users.noreply.github.com>
2024-11-01 01:08:37 +0100

    home responsive

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 
 function Header({ toggleSidebar }) {
   return (
-    <header className="relative p-10 text-center shadow-lg overflow-hidden">
+    <header className="p-2 sm:p-10 text-center shadow-lg ">
       <button onClick={toggleSidebar} className="text-2xl absolute top-3 left-3 sm:top-6 sm:left-6">
         ☰
       </button>
       <div className="flex items-center justify-center space-x-4 animate-fade-in">
-        <img src="./src/assets/logo.png" className="w-20 animate-bounce" alt="Logo" />
-        <h1 className="text-6xl font-Sriracha text-blue-900 tracking-wider">Mathify</h1>
+        <img src="./src/assets/logo.png" className="w-16 sm:w-20 animate-bounce" alt="Logo" />
+        <h1 className="text-5xl sm:text-6xl font-Sriracha text-blue-900 tracking-wider">Mathify</h1>
       </div>
       <p className="mt-5 text-2xl text-gray-700">Train Your Math Skills</p>
       <div className="absolute inset-0 pointer-events-none z-[-1] bg-gradient-to-r from-white-300/50 via-transparent to-gray-300/50"></div>
--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -2,9 +2,9 @@ import { FaLongArrowAltRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
 function Home() {
   return (
-    <div className="flex justify-center gap-5 mt-10">
+    <div className="flex justify-center gap-2 sm:gap-5 mx-2 sm:mt-10 mt-3 flex-col sm:flex-row items-center overflow-auto">
       <Link to="/login">
-        <div className="cursor-pointer w-48 bg-gray-300 font-Inconsolata text- h-[200px] rounded shadow-2xl border-2 border-gray-900/35 hover:shadow-[inset_-12px_-8px_40px_#46464620]">
+        <div className="cursor-pointer sm:w-48 max-w-[400px] bg-gray-300 font-Inconsolata   rounded shadow-2xl border-2 border-gray-900/35 hover:shadow-[inset_-12px_-8px_40px_#46464620]">
           <h1 className="text-center font-extrabold bg-gray-400/30">Log in</h1>
           <p className="text-center mt-2">to get the full version, check your stats and level up... </p>
           <span className="flex justify-center">
@@ -13,7 +13,7 @@ function Home() {
         </div>
       </Link>
       <Link to="/game">
-        <div className="cursor-pointer w-48 bg-gray-300 font-Inconsolata text- h-[200px] rounded shadow-2xl border-2 border-gray-900/35 hover:shadow-[inset_-12px_-8px_40px_#46464620]">
+        <div className="cursor-pointer sm:w-48 max-w-[400px] bg-gray-300 font-Inconsolata   rounded shadow-2xl border-2 border-gray-900/35 hover:shadow-[inset_-12px_-8px_40px_#46464620]">
           <h1 className="text-center font-extrabold bg-gray-400/30">Play</h1>
           <p className="text-center mt-2"> Solve math problems. one game consists of 5 equations... </p>
           <span className="flex justify-center">
@@ -22,7 +22,7 @@ function Home() {
         </div>
       </Link>
       <Link to="/stats">
-        <div className="cursor-pointer w-48 bg-gray-300 font-Inconsolata text- h-[200px] rounded shadow-2xl border-2 border-gray-900/35 hover:shadow-[inset_-12px_-8px_40px_#46464620]">
+        <div className="cursor-pointer sm:w-48 max-w-[400px] bg-gray-300 font-Inconsolata  rounded shadow-2xl border-2 border-gray-900/35 hover:shadow-[inset_-12px_-8px_40px_#46464620]">
           <h1 className="text-center font-extrabold bg-gray-400/30">Stats</h1>
           <p className="text-center mt-2">check your statistics, how quickly you solve tasks and more...</p>
           <span className="flex justify-center">
