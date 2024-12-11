7f71245ea551f4b428d7a87be95412da20d1788f
Mati <byku00pl@gmail.com>
2024-12-11 18:46:04 +0100

    Delete .env and created Dockerfile

--- src/main/java/com/project/mathtrainer/MathTrainerApplication.java
+++ src/main/java/com/project/mathtrainer/MathTrainerApplication.java
@@ -1,15 +1,12 @@
 package com.project.mathtrainer;
 
 import org.springframework.boot.SpringApplication;
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MathTrainerApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("spring.datasource.url", dotenv.get("database_url"));
 		SpringApplication.run(MathTrainerApplication.class, args);
 
 	}
