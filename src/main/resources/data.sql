cd9ad976ea5a78f85303be8add38250dde8c7527
MarcinGarcin <knucias123@gmail.com>
2024-12-11 13:00:07 +0100

    Added enviromental variables , changed database to supabase

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO "user" (id, username, password, email, is_verified, lvl, questions_on_this_lvl) VALUES
-(0, 'admin', '$2a$10$NXK9XB1DsWzWl4b.YZkqPOQgCZKws5GlhjyZ8OG75Q2980hU8sUni', 'test1@example.com', true, 1, 0);
-
-INSERT INTO "stat" (id, wrong_answers, total_questions, average_time_per_question, total_time, date,  user_id) VALUES
-(0, 5, 5, 3.4, 12.9, '13-05-2024', 0);
